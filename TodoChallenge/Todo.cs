b372d1e41f7a4d538d89d2108f06c9d32762de5a
Martin Tamm <mtammm@gmail.com>
2019-08-13 16:23:13 +0300

    todo excercise

---
+++ TodoChallenge/Todo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoChallenge
+{
+    public class Todo
+    {
+        public string description { get; set; }
+        public List<string> todoList { get; set; }
+
+        public  void Add(string description)
+        {
+            todoList.Add(description);
+        }
+
+        public  void Print()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(todoList[i]);
+            }
+        }
+
+        public void printAll()
+        {
+            foreach(var item in todoList)
+            {
+
+            }
+        }
+
+
+    }
+}
